85bb50c4fb55ada5da83be811b627fb561b01488
Sehyeon Park <nesmile23@gmail.com>
2023-06-22 21:12:35 +0900

    feat: menus domain 추가, service test code 초안 작성

--- src/app-router.module.ts
+++ src/app-router.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@nestjs/core';
 import { OrdersModule } from 'src/main/order/orders.module';
 import { AppModule } from './app.module';
 import { AlertsModule } from './main/alert/alerts.module';
+import { MenusModule } from './menus/menus.module';
 
 const routes: Routes = [
   { path: 'orders', module: OrdersModule },
@@ -13,7 +14,8 @@ const routes: Routes = [
   imports: [
     OrdersModule,
     AlertsModule,
-    RouterModule.register(routes)
+    RouterModule.register(routes),
+    MenusModule,
   ],
 })
 export class AppRouterModule {}
---
+++ src/menus/menus.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MenusService {
+  create(data) {}
+  getAll(): [] {
+    return [];
+  }
+  getOne(id) {
+    return {};
+  }
+  update(id, data) {}
+  delete(id) {}
+}
