4df315dd16ac8e1a3e9036661172c3e087830c53
Sehyeon Park <nesmile23@gmail.com>
2023-07-01 02:27:41 +0900

    chore: menus 폴더 이동 및 controller 제거

--- src/main/menus/menus.module.ts
+++ src/main/menus/menus.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
-import { MenusController } from './menus.controller';
 
 @Module({
   providers: [MenusService],
-  controllers: [MenusController],
 })
 export class MenusModule {}
