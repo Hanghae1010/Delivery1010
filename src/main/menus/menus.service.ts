e42cd7cf1becfcf004d9be6b7f627e6a43101214
Sehyeon Park <nesmile23@gmail.com>
2023-07-01 02:32:24 +0900

    feat: menus service 추가 및 생성 메소드 구현

--- src/main/menus/menus.service.ts
+++ src/main/menus/menus.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Menu } from './entities/menu.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class MenusService {
-  create(data) {}
+  constructor(@InjectRepository(Menu) private repo: Repository<Menu>) {}
+
+  create(name: string, price: number, storeId: number) {
+    const menu = this.repo.create({
+      name,
+      price,
+      store: { id: storeId },
+    });
+    return this.repo.save(menu);
+  }
   getAll(): [] {
     return [];
   }
   getOne(id) {
     return {};
   }
-  update(id, data) {}
-  delete(id) {}
+  update(id, data) {
+    return {};
+  }
+  delete(id) {
+    return {};
+  }
 }
