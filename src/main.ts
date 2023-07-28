676e92aaba8674903924daf2ac6b8314f090ce53
Sehyeon Park <nesmile23@gmail.com>
2023-07-28 19:00:59 +0900

    fix: bootstrap에 logger middleware 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from 'src/utils/swagger';
+import logger from './utils/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
